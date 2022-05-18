1b6d2b4b33ce4cd94f7202648802489a651b5ace
Hee Jae <dark199174@gmail.com>
2022-05-18 17:23:17 +0900

    Add :: frontend

---
+++ week2/BuyMeABubbleTea-front/pages/index.jsx
@@ -0,0 +1,257 @@
+import abi from '../utils/BuyMeABubbleTea.json';
+import { ethers } from "ethers";
+import Head from 'next/head'
+import Image from 'next/image'
+import React, { useEffect, useState } from "react";
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  // Contract Address & ABI
+  const contractAddress = "0x133394e78B6C0cD2F6ea57FFfBf930768967f827";
+  const contractABI = abi.abi;
+
+  // Component state
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [memos, setMemos] = useState([]);
+
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const onMessageChange = (event) => {
+    setMessage(event.target.value);
+  }
+
+  // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({method: 'eth_accounts'})
+      console.log("accounts: ", accounts);
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log("please install MetaMask");
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const buyBubbleTea = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+        const buyMeABubbleTea = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        console.log("buying bubble tea..")
+        const bubbleTeaTxn = await buyMeABubbleTea.buyBubbleTea(
+          name ? name : "Santa",
+          message ? message : "Enjoy your bubble tea!",
+          {value: ethers.utils.parseEther("0.001")}
+        );
+
+        await bubbleTeaTxn.wait();
+
+        console.log("mined ", bubbleTeaTxn.hash);
+
+        console.log("bubble tea purchased!");
+
+        // Clear the form fields.
+        setName("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Function to fetch all memos stored on-chain.
+  const getMemos = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const buyMeABubbleTea = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        
+        console.log("fetching memos from the blockchain..");
+        const memos = await buyMeABubbleTea.getMemos();
+        console.log("fetched!");
+        setMemos(memos);
+      } else {
+        console.log("Metamask is not connected");
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    let buyMeABubbleTea;
+    isWalletConnected();
+    getMemos();
+
+    // Create an event handler function for when someone sends
+    // us a new memo.
+    const onNewMemo = (from, timestamp, name, message) => {
+      console.log("Memo received: ", from, timestamp, name, message);
+      setMemos((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message,
+          name
+        }
+      ]);
+    };
+
+    const {ethereum} = window;
+
+    // Listen for new memo events.
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const signer = provider.getSigner();
+      buyMeABubbleTea = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      buyMeABubbleTea.on("NewMemo", onNewMemo);
+    }
+
+    return () => {
+      if (buyMeABubbleTea) {
+        buyMeABubbleTea.off("NewMemo", onNewMemo);
+      }
+    }
+  }, []);
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buy HeJ a Bubble Tea!</title>
+        <meta name="description" content="Tipping site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Buy HeJ a Bubble Tea!
+        </h1>
+        
+        {currentAccount ? (
+          <div>
+            <form>
+              <div>
+                <label>
+                  Name
+                </label>
+                <br/>
+                
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="Santa"
+                  value={name}
+                  onChange={onNameChange}
+                  />
+              </div>
+              <br/>
+              <div>
+                <label>
+                  Send HeJ a message
+                </label>
+                <br/>
+
+                <textarea
+                  rows={3}
+                  placeholder="Enjoy your bubble tea!"
+                  id="message"
+                  value={message}
+                  onChange={onMessageChange}
+                  required
+                >
+                </textarea>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  onClick={buyBubbleTea}
+                >
+                  Send 1 Bubble Tea for 0.001ETH
+                </button>
+                <button
+                  type="button"
+                  onClick={buyBubbleTea}
+                >
+                  Send 1 Large Bubble Tea for 0.003ETH
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <button onClick={connectWallet}> Connect your wallet </button>
+        )}
+      </main>
+
+      {currentAccount && (<h1>Memos received</h1>)}
+
+      {currentAccount && (memos.map((memo, idx) => {
+        return (
+          <div key={idx} style={{border:"2px solid", "borderRadius":"5px", padding: "5px", margin: "5px"}}>
+            <p style={{"fontWeight":"bold"}}>"{memo.message}"</p>
+            <p>From: {memo.name} at {memo.timestamp.toString()}</p>
+          </div>
+        )
+      }))}
+
+      <footer className={styles.footer}>
+        <a
+          href="https://alchemy.com/?a=roadtoweb3weektwo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by @hjplumtree for Alchemy's Road to Web3 lesson two!
+        </a>
+      </footer>
+    </div>
+  )
+}
