48e594aa105f8ba9caed6704d3477510ffc44343
Hee Jae <dark199174@gmail.com>
2022-05-19 22:46:24 +0900

    Add :: week2 url

---
+++ week2/_next/static/FJjRv_M0R_d39bkUg-1pN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/609-b892b6957791b92f.js","static/css/f36a37245ec89372.css","static/chunks/pages/index-f419edf18f19b421.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
